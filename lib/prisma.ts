c6ae2e614845fead6d20af04f498817389e9182a
Rohitlodhii <rohitlodhi0225@gmail.com>
2024-10-29 11:18:36 +0530

    setup prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,21 @@
+//Connecting the PrismaClient to the app
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSinglton = () => {
+    return new PrismaClient();
+}
+
+type prismaClientSinglton = ReturnType<typeof prismaClientSinglton>;
+
+const globalForPrisma = globalThis as unknown as { prisma : PrismaClient | undefined};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSinglton();
+
+
+export default prisma ;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma ;
+
+
+
